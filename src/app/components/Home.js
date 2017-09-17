5d4fa178d23347df23dd77fee6ecd07d3c5c4504
yyang325 <frank.young325@gmail.com>
2017-09-17 16:11:12 -0700

    dynamic content defined between {}.

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,14 @@ import React from "react";
 
 export class Home extends React.Component {
 	render() {
+		let content = "";
+		if (true) {
+			content = "This is a Dynamic content. ";
+		}
 		return (
 			<div>
 				<p> This is a new Component</p>
+				{ content }
 			</div>
 		);
 	}
